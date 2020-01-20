c18f834b3f8482189282e40f7f886b7014f3dc76
Simon Garber <Simon.garber@gmail.com>
2020-01-20 18:32:32 -0500

    adjusted path in router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
-import GetCards from "./components/GetCards/GetCards";
-import Cards from "./components/Cards/Cards";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider as AuthProvider } from "./stateManagement/AuthContext";
 import { Provider as UserCardsProvider } from "./stateManagement/userCardsContext";
 import WithAuth from "./components/WithAuth/WithAuth";
+import GetCards from "./components/GetCards/GetCards";
+import Cards from "./components/Cards/Cards";
 import SignIn from "./components/SignIn/SignIn";
 import SignUp from "./components/SignUp/SignUp";
 
@@ -16,11 +16,11 @@ const App = () => {
         <div>
           <Router>
             <Switch>
-              <Route path="/cards" exact component={WithAuth(Cards)} />
-              <Route path="/search" exact component={WithAuth(GetCards)} />
-              <Route path="/" exact component={SignIn} />
-              <Route path="/signin" exact component={SignIn} />
-              <Route path="/signup" exact component={SignUp} />
+              <Route exact path="/" component={SignIn} />
+              <Route exact path="/signin" component={SignIn} />
+              <Route exact path="/signup" component={SignUp} />
+              <Route exact path="/search" component={WithAuth(GetCards)} />
+              <Route exact path="/cards" component={WithAuth(Cards)} />
             </Switch>
           </Router>
         </div>
